abf4aa4d1dab20fb42471bff1770af796df3f69c
robiulhasananim <mdrobiulhasananim@gmail.com>
2025-11-13 13:40:21 +0600

    fix: fix some drf warnning

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name="profile"),
     path('changepassword/', UserChangePasswordView.as_view(), name="changepassword"),
     path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name="send-reset-password-email"),
-    path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name="reset-password"),
+    path('reset-password/<str:uid>/<str:token>/', UserPasswordResetView.as_view(), name="reset-password"),
     path('logout/', LogoutView.as_view(), name="logout"),
 ]
